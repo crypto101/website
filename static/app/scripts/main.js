e198c8e38f29113bed53a8d8d54901598fa945ff
Laurens Van Houtven <_@lvh.cc>
2014-02-27 14:21:19 +0100

    Some JS code for e-mail submission

--- static/app/scripts/main.js
+++ static/app/scripts/main.js
@@ -2,3 +2,34 @@ $("#download-link").click(function () {
   $(this).find("button").attr("disabled", true);
   $("#email-form").show();
 });
+
+#("#submit-email").click(function () {
+  $("#submit-email").attr("disabled", true);
+  $.ajax("https://crypto101.io/subscribe", {
+    "data": {
+      "address": $("#email").val(),
+      "force": false
+    },
+    "dataType": "json"
+  }).done(function(data) {
+    if (data.success === true) {
+      $("email-message").text("Thanks, you're signed up!");
+    }
+    else {
+      if (data.reason === "duplicate") {
+        $("email-message").text("Looks like you're already signed up :-)");
+      }
+      else if (data.reason === "invalid") {
+        var msg = "That address doesn't look quite right.";
+        if (data.suggestion !== null) {
+          msg += " Did you mean " + data.suggestion + "?"
+        }
+        $("email-message").text(msg);
+        $("#submit-email").attr("disabled", false);
+      }
+      else {
+        $("email-message").text("Something appears to be broken :-(");
+      }
+    }
+  })
+});
