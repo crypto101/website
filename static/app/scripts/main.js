0970dc7ec1f00b04cc6d2a9d7001a6f78ee079c5
Laurens Van Houtven <_@lvh.cc>
2014-02-28 11:33:25 +0100

    Fix submission stuff

--- static/app/scripts/main.js
+++ static/app/scripts/main.js
@@ -1,35 +1,39 @@
-$("#download-link").click(function () {
-  $(this).find("button").attr("disabled", true);
-  $("#email-form").show();
-});
+$(function() {
+  var downloadLink = $("#download-link");
 
-#("#submit-email").click(function () {
-  $("#submit-email").attr("disabled", true);
-  $.ajax("https://crypto101.io/subscribe", {
-    "data": {
-      "address": $("#email").val(),
-      "force": false
-    },
-    "dataType": "json"
-  }).done(function(data) {
-    if (data.success === true) {
-      $("email-message").text("Thanks, you're signed up!");
-    }
-    else {
-      if (data.reason === "duplicate") {
-        $("email-message").text("Looks like you're already signed up :-)");
-      }
-      else if (data.reason === "invalid") {
-        var msg = "That address doesn't look quite right.";
-        if (data.suggestion !== null) {
-          msg += " Did you mean " + data.suggestion + "?"
-        }
-        $("email-message").text(msg);
-        $("#submit-email").attr("disabled", false);
+  downloadLink.click(function () {
+    downloadLink.find("button").attr("disabled", "disabled");
+    $("#email-form").show();
+  });
+
+  $("#submit-email").click(function () {
+    $("#submit-email").attr("disabled", "disabled");
+    $.ajax("https://crypto101.io/subscribe", {
+      "data": {
+        "address": $("#email").val(),
+        "force": false
+      },
+      "dataType": "json"
+    }).done(function(data) {
+      if (data.success === true) {
+        $("email-message").text("Thanks, you're signed up!");
       }
       else {
-        $("email-message").text("Something appears to be broken :-(");
+        if (data.reason === "duplicate") {
+          $("email-message").text("Looks like you're already signed up :-)");
+        }
+        else if (data.reason === "invalid") {
+          var msg = "That address doesn't look quite right.";
+          if (data.suggestion !== null) {
+            msg += " Did you mean " + data.suggestion + "?"
+          }
+          $("email-message").text(msg);
+          $("#submit-email").attr("disabled", false);
+        }
+        else {
+          $("email-message").text("Something appears to be broken :-(");
+        }
       }
-    }
-  })
+    })
+  });
 });
