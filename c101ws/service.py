083d70d724c40e20cb3d5b14c1d292b645cdd3d1
Laurens Van Houtven <_@lvh.cc>
2014-03-04 18:34:06 +0100

    Add tests and test certificate, fix some bugs

--- c101ws/service.py
+++ c101ws/service.py
@@ -5,14 +5,17 @@ from twisted.application.service import Service, IServiceMaker
 from twisted.internet import reactor
 from twisted.internet.endpoints import SSL4ServerEndpoint, TCP4ServerEndpoint
 from twisted.internet.ssl import PrivateCertificate
+from twisted.plugin import IPlugin
 from twisted.python.usage import Options
 from zope.interface import implementer
 
 
 class Service(Service):
-    def __init__(self):
+    def __init__(self, _environ=environ):
         self._httpEndpoint = TCP4ServerEndpoint(reactor, 80)
-        cert = PrivateCertificate.loadPEM(environ.get("CERTIFICATE"))
+        with open(_environ["CERTIFICATE_PATH"]) as f:
+            pemData = f.read()
+        cert = PrivateCertificate.loadPEM(pemData)
         ctxFactory = SecureCiphersContextFactory(cert.options())
         self._tlsEndpoint = SSL4ServerEndpoint(reactor, 443, ctxFactory)
 
@@ -23,7 +26,7 @@ class Service(Service):
 
 
 
-@implementer(IServiceMaker)
+@implementer(IServiceMaker, IPlugin)
 class ServiceMaker(object):
     tapname = "c101ws"
     options = Options
---
+++ c101ws/test/test_service.py
@@ -0,0 +1,29 @@
+from os.path import dirname, join
+from c101ws.service import Service, ServiceMaker
+from twisted.application.service import IService, IServiceMaker
+from twisted.plugin import IPlugin
+from twisted.trial.unittest import SynchronousTestCase
+from zope.interface.verify import verifyObject
+
+
+class ServiceTests(SynchronousTestCase):
+    def setUp(self):
+        certPath = join(dirname(__file__), "test-cert-chain.pem")
+        self.service = Service(_environ={"CERTIFICATE_PATH": certPath})
+
+
+    def test_interfaces(self):
+        """The service class implements IService.
+
+        """
+        verifyObject(IService, self.service)
+
+
+class ServiceMakerTests(SynchronousTestCase):
+    def test_interfaces(self):
+        """The serivce maker implements both IServiceMaker and IPlugin.
+
+        """
+        sm = ServiceMaker()
+        verifyObject(IServiceMaker, sm)
+        verifyObject(IPlugin, sm)
