7706062a8cf57bcf1f69d60e9aab654de6df3aed
Laurens Van Houtven <_@lvh.cc>
2014-03-11 14:32:33 +0100

    Add some basic tests for context factories
    Closes #5.

--- c101ws/test/test_service.py
+++ c101ws/test/test_service.py
@@ -1,5 +1,7 @@
-from os.path import dirname, join
 from c101ws.service import WebsiteService, WebsiteServiceMaker
+from clarent.certificate import SecureCiphersContextFactory
+from OpenSSL.SSL import SSLv23_METHOD
+from os.path import dirname, join
 from twisted.application.service import IService, IServiceMaker
 from twisted.plugin import IPlugin
 from twisted.trial.unittest import SynchronousTestCase
@@ -10,6 +12,23 @@ class ServiceTests(SynchronousTestCase):
     def setUp(self):
         certPath = join(dirname(__file__), "test-cert-chain.pem")
         self.service = WebsiteService(_environ={"CERTIFICATE_PATH": certPath})
+
+
+    def test_contextFactory(self):
+        """The context factory produces pragmatic, secure SSL contexts.
+
+        The context factory is a ``SecureCiphersContextFactory``
+        wrapping an ``OpenSSLCertificateOptions`` that uses
+        ``SSLv23_METHOD``.
+
+        The context factory can produce contexts.
+        """
+        ctxFactory = self.service._getCtxFactory()
+        self.assertIdentical(type(ctxFactory), SecureCiphersContextFactory)
+        certOptions = ctxFactory.ctxFactory
+        self.assertEqual(certOptions.method, SSLv23_METHOD)
+
+        ctxFactory.getContext()
 
 
     def test_interfaces(self):
