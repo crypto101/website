1d5ecd9e2f5ad586eddfc7e2e337635425a19205
Laurens Van Houtven <_@lvh.cc>
2014-03-11 12:14:55 +0100

    pem: It Does What You Want(TM)

--- c101ws/service.py
+++ c101ws/service.py
@@ -2,26 +2,13 @@ from c101ws.web import secureSite, insecureSite
 from clarent.certificate import SecureCiphersContextFactory
 from os import environ
 from OpenSSL.SSL import SSLv23_METHOD
+from pem import certificateOptionsFromFiles
 from twisted.application.service import Service, IServiceMaker
 from twisted.internet import reactor
 from twisted.internet.endpoints import SSL4ServerEndpoint, TCP4ServerEndpoint
-from twisted.internet.ssl import PrivateCertificate
 from twisted.plugin import IPlugin
 from twisted.python.usage import Options
 from zope.interface import implementer
-
-
-class CertChainContextFactory(object):
-    def __init__(self, ctxFactory, certChainPath):
-        self.ctxFactory = ctxFactory
-        self.certChainPath = certChainPath
-
-
-    def getContext(self):
-        ctx = self.ctxFactory.getContext()
-        ctx.use_certificate_chain_file(self.certChainPath)
-        return ctx
-
 
 
 class WebsiteService(Service):
@@ -40,14 +27,10 @@ class WebsiteService(Service):
 
     def _getCtxFactory(self):
         certPath = self._environ["CERTIFICATE_PATH"]
-        with open(certPath) as f:
-            pemData = f.read()
-
-        ctxFactory = PrivateCertificate.loadPEM(pemData).options()
-        ctxFactory.method = SSLv23_METHOD
+        ctxFactory = certificateOptionsFromFiles(certPath,
+            method=SSLv23_METHOD)
 
         ctxFactory = SecureCiphersContextFactory(ctxFactory)
-        ctxFactory = CertChainContextFactory(ctxFactory, certPath)
 
         theContext = ctxFactory.getContext()
         return lambda: theContext
--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ dependencies = [
     "twisted>=13.2.0",
     "clarent>=0.1.1",
     "PyOpenSSL>=0.14",
-    "treq==0.2.0"
+    "treq==0.2.0",
+    "pem==0.1.0"
 ]
 
 import re
