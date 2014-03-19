2c3cccdb992af3f9abb041fd46f7cf45605ee677
Laurens Van Houtven <_@lvh.cc>
2014-03-19 19:41:40 +0100

    CSS selectors: how do they work?

--- static/app/scripts/main.js
+++ static/app/scripts/main.js
@@ -16,25 +16,26 @@ $(function() {
       },
       "dataType": "json"
     }).done(function(data) {
+      console.log("data");
       if (data.success === true) {
-        $("email-message").text("Thanks, you're signed up!");
+        $("#email-message").text("Thanks, you're signed up!");
       }
       else {
         if (data.reason === "duplicate") {
-          $("email-message").text("Looks like you're already signed up :-)");
+          $("#email-message").text("Looks like you're already signed up :-)");
         }
         else if (data.reason === "invalid") {
           var msg = "That address doesn't look quite right.";
           if (data.suggestion !== null) {
             msg += " Did you mean " + data.suggestion + "?"
           }
-          $("email-message").text(msg);
+          $("#email-message").text(msg);
           $("#submit-email").attr("disabled", false);
         }
         else {
-          $("email-message").text("Something appears to be broken :-(");
+          $("#email-message").text("Something appears to be broken :-(");
         }
       }
-    })
+    });
   });
 });
